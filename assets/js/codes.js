a606dd5ccdaeafee7850c32474e297560ada1b43
Franco Calegari <francocalegari55@gmail.com>
2024-08-05 17:08:00 -0300

    fixes

--- assets/js/codes.js
+++ assets/js/codes.js
@@ -28,14 +28,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const inactiveCodesContainer = document.getElementById('inactive-codes');
     const toggleInactiveButton = document.getElementById('toggle-inactive');
     const popup = document.getElementById('popup');
-
+    
     function showPopup() {
         popup.classList.add('code-show');
         setTimeout(() => {
             popup.classList.remove('code-show');
         }, 2000);
     }
-
+    
     function updateRemainingTime() {
         const now = new Date();
         codes.forEach(code => {
@@ -43,14 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
             if (code.type === "temporal") {
                 expirationDate.setDate(expirationDate.getDate() + TEMPORAL_DAYS);
                 const difference = expirationDate.getTime() - now.getTime();
-
+    
                 const card = document.getElementById(code.text);
                 if (!card) return; // Skip if the card is not found
-
+    
                 const expiraEnElement = card.querySelector('.expira-en');
                 const remainingTimeElement = card.querySelector('.remaining-time');
                 if (!expiraEnElement || !remainingTimeElement) return; // Skip if the elements are not found
-
+    
                 if (difference > 0) { // Si el código todavía no ha expirado
                     expiraEnElement.textContent = 'Expira en:';
                     if (difference > 86400000) { // Si es mayor a 24 horas
@@ -69,22 +69,22 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-
+    
     setInterval(updateRemainingTime, 1000); // Actualizar cada segundo
-
+    
     // Ordenar los códigos por fecha en orden descendente
     codes.sort((a, b) => new Date(b.date) - new Date(a.date));
-
+    
     // Obtener la fecha más reciente
     const mostRecentDate = codes[0].date;
-
+    
     const inactiveCodes = [];
     let hasActiveCodes = false;
-
+    
     codes.forEach(code => {
         const issuedDate = new Date(code.date);
         let isActive = false;
-
+    
         if (code.type === "permanente") {
             isActive = true;
         } else if (code.type === "temporal") {
@@ -92,38 +92,38 @@ document.addEventListener('DOMContentLoaded', () => {
             expirationDate.setDate(issuedDate.getDate() + TEMPORAL_DAYS);
             isActive = today <= expirationDate;
         }
-
+    
         const codeElement = document.createElement('div');
         codeElement.id = code.text; // Usar el código como ID para actualizar el tiempo restante
         codeElement.className = 'code-card';
-
+    
         // Resaltar el código con la fecha más reciente
         if (code.date === mostRecentDate) {
             codeElement.style.filter = 'drop-shadow(1px 1px 30px #cab65e)';
             codeElement.style.transition = '500ms all';
             codeElement.style.border = 'solid 3px #efcd36';
         }
-
+    
         const codeText = document.createElement('p');
         codeText.textContent = code.text;
         codeElement.appendChild(codeText);
-
+    
         const releaseDate = document.createElement('p');
         releaseDate.textContent = `Emitido: ${issuedDate.toLocaleDateString()}`;
         codeElement.appendChild(releaseDate);
-
+    
         if (isActive) {
             hasActiveCodes = true;
             if (code.type === "temporal") {
                 const expiraEn = document.createElement('p');
                 expiraEn.className = 'expira-en';
                 codeElement.appendChild(expiraEn);
-
+    
                 const remainingTime = document.createElement('p');
                 remainingTime.className = 'remaining-time';
                 codeElement.appendChild(remainingTime);
             }
-
+    
             codeElement.addEventListener('click', () => {
                 navigator.clipboard.writeText(code.text).then(() => {
                     showPopup();
@@ -140,23 +140,23 @@ document.addEventListener('DOMContentLoaded', () => {
             inactiveCodes.push(codeElement);
         }
     });
-
+    
     if (!hasActiveCodes) {
         const noActiveCodesMessage = document.createElement('p');
         noActiveCodesMessage.textContent = "No hay códigos activos por el momento :(";
         activeCodesContainer.appendChild(noActiveCodesMessage);
     }
-
+    
     // Mostrar los primeros 4 códigos inactivos
     inactiveCodes.slice(0, 4).forEach(codeElement => {
         inactiveCodesContainer.appendChild(codeElement);
     });
-
+    
     // Mostrar botón si hay más de 4 códigos inactivos
     if (inactiveCodes.length > 4) {
         toggleInactiveButton.classList.remove('hidden');
         let showingMore = false;
-
+    
         toggleInactiveButton.addEventListener('click', () => {
             if (showingMore) {
                 // Ocultar los códigos inactivos extras
@@ -175,10 +175,10 @@ document.addEventListener('DOMContentLoaded', () => {
             showingMore = !showingMore;
         });
     }
-
+    
     updateRemainingTime(); // Initial call to show remaining time immediately
-
-    //// Verificar si el navegador soporta notificaciones
+    
+    // Verificar si el navegador soporta notificaciones
     if (!("Notification" in window)) {
         alert("Este navegador no soporta notificaciones de escritorio.");
     } else {
@@ -189,11 +189,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Permiso concedido
                     console.log("Permiso para notificaciones concedido.");
                     verificarNuevosCodigos(); // Ejecutar la verificación al habilitar notificaciones
+    
+                    // Agregar animación de parpadeo
+                    document.getElementById('enable-notifications').classList.add('blink');
                 }
             });
         });
     }
-
+    
     // Función para mostrar una notificación
     function mostrarNotificacion(titulo, mensaje, url) {
         if (Notification.permission === 'granted') {
@@ -208,26 +211,26 @@ document.addEventListener('DOMContentLoaded', () => {
             };
         }
     }
-
+    
     // Función para contar códigos válidos (no expirados)
     function contarCodigos() {
         return codes.filter(code => code.type !== 'expired').length;
     }
-
+    
     // Almacenar la cantidad inicial de códigos válidos
     if (!localStorage.getItem('cantidadCodigos')) {
         localStorage.setItem('cantidadCodigos', contarCodigos());
     }
-
+    
     // Función para verificar si hay nuevos códigos
     function verificarNuevosCodigos() {
         const cantidadGuardada = parseInt(localStorage.getItem('cantidadCodigos'), 10);
         const cantidadActual = contarCodigos();
-
+    
         // Log en consola las variables del caché y las actuales
         console.log(`Cantidad de códigos guardada en caché: ${cantidadGuardada}`);
         console.log(`Cantidad de códigos actual en la página: ${cantidadActual}`);
-
+    
         if (cantidadActual > cantidadGuardada) {
             const titulo = "¡Nuevo Código de Tsuki :D!";
             const mensaje = `Hay ${cantidadActual - cantidadGuardada} nuevos códigos de canje disponibles.`;
@@ -238,7 +241,7 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.setItem('cantidadCodigos', cantidadActual);
         }
     }
-
+    
     // Verificar nuevos códigos cada 10 segundos
     setInterval(verificarNuevosCodigos, 10000);
-});
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Variables y funciones para el manejo de los artículos
-    const itemListContainer = document.getElementById('itemListContainer');
-    const searchInput = document.getElementById('searchInput');
-    const filterSelect = document.getElementById('filterSelect');
-    const filterButton = document.getElementById('filterButton');
 
-    function csvToArray(str, delimiter = ",") {
-        const headers = str.slice(0, str.indexOf("\n")).split(delimiter).map(header => header.trim());
-        const rows = str.slice(str.indexOf("\n") + 1).split("\n");
 
-        const items = rows.map(row => {
-            const values = row.split(delimiter).map(value => value.trim());
-            if (values.length !== headers.length || values.includes('')) {
-                return null;
-            }
-            const item = headers.reduce((object, header, index) => {
-                object[header] = values[index];
-                return object;
-            }, {});
-            return item;
-        }).filter(item => item !== null);
+// Variables y funciones para el manejo del mapa
+const pins = document.querySelectorAll('.pin');
+const overlay = document.getElementById('overlay');
+const pinInfoBox = document.getElementById('pin-info-box');
+const pinTitle = document.getElementById('pin-title');
+const pinDescription = document.getElementById('pin-description');
+const commonFishList = document.getElementById('common-fish-list');
+const legendaryFishList = document.getElementById('legendary-fish-list');
+const closeBtn = document.querySelector('.close-btn');
 
-        return items;
-    }
+pins.forEach(pin => {
+    pin.addEventListener('click', () => {
+        const title = pin.getAttribute('data-title');
+        const description = pin.getAttribute('data-description');
+        const commonFish = pin.getAttribute('data-common-fish').split(', ');
+        const legendaryFish = pin.getAttribute('data-legendary-fish').split(', ');
 
-    function renderItemsByCategory(itemsToRender) {
-        const categories = [...new Set(itemsToRender.map(item => item.category))];
-        itemListContainer.innerHTML = '';
-    
-        categories.forEach(category => {
-            const section = document.createElement('section');
-            section.classList.add('category-section');
-            section.innerHTML = `<div class="categoria-titulo"><h2>${category}</h2></div>`;
-            const itemList = document.createElement('div');
-            itemList.classList.add('item-list');
-    
-            const itemsInCategory = itemsToRender.filter(item => item.category === category);
-            itemsInCategory.forEach(item => {
-                const itemElement = document.createElement('div');
-                itemElement.classList.add('item', 'fade-in');
-                itemElement.innerHTML = `
-                    <img src="${item.image}" alt="${item.name}">
-                    <div class="item-details">
-                        <div class="item-name">${item.name}</div>
-                        <div class="item-price"><img src="https://cdn-icons-png.flaticon.com/512/2556/2556893.png" style="width: 20px; height: fit-content; border-radius: 1px; padding: 0; margin: 0; background-color: transparent; border-radius: 0;">${item.price}</div>
-                        <div class="item-category">Categoría: ${item.category}</div>
-                        <div class="item-rarity">Rareza: ${item.rarity}</div>
-                        <div class="more-info">${item.description}</div>
-                    </div>
-                `;
-    
-                // Agregar clase de rareza para asignar el color de fondo
-                switch (item.rarity.toLowerCase()) {
-                    case 'común':
-                        itemElement.classList.add('common');
-                        break;
-                    case 'raro':
-                        itemElement.classList.add('rare');
-                        break;
-                    case 'legendario':
-                        itemElement.classList.add('legendary');
-                        break;
-                    default:
-                        break;
-                }
-    
-                itemElement.addEventListener('click', () => {
-                    itemElement.classList.toggle('active');
-                });
-                itemList.appendChild(itemElement);
-            });
-    
-            section.appendChild(itemList);
-            itemListContainer.appendChild(section);
+        pinTitle.textContent = title;
+        pinDescription.textContent = description;
+
+        commonFishList.innerHTML = '';
+        legendaryFishList.innerHTML = '';
+
+        commonFish.forEach(fish => {
+            const li = document.createElement('li');
+            li.textContent = fish;
+            commonFishList.appendChild(li);
         });
-    }
 
-    function fetchItems() {
-        fetch('items.csv')
-            .then(response => response.text())
-            .then(data => {
-                const items = csvToArray(data);
-                renderItemsByCategory(items);
-
-                searchInput.addEventListener('input', () => {
-                    const searchTerm = searchInput.value.toLowerCase();
-                    const filteredItems = items.filter(item => item.name.toLowerCase().includes(searchTerm));
-                    renderItemsByCategory(filteredItems);
-                });
-
-                filterButton.addEventListener('click', () => {
-                    const filterTerm = filterSelect.value;
-                    const filteredItems = items.filter(item => item.category.toLowerCase().includes(filterTerm.toLowerCase()) || item.rarity.toLowerCase().includes(filterTerm.toLowerCase()));
-                    renderItemsByCategory(filteredItems);
-                });
-            })
-            .catch(error => console.error('Error al cargar el archivo CSV:', error));
-    }
-
-    fetchItems();
-
-    // Variables y funciones para el manejo del mapa
-    const pins = document.querySelectorAll('.pin');
-    const overlay = document.getElementById('overlay');
-    const pinInfoBox = document.getElementById('pin-info-box');
-    const pinTitle = document.getElementById('pin-title');
-    const pinDescription = document.getElementById('pin-description');
-    const commonFishList = document.getElementById('common-fish-list');
-    const legendaryFishList = document.getElementById('legendary-fish-list');
-    const closeBtn = document.querySelector('.close-btn');
-
-    pins.forEach(pin => {
-        pin.addEventListener('click', () => {
-            const title = pin.getAttribute('data-title');
-            const description = pin.getAttribute('data-description');
-            const commonFish = pin.getAttribute('data-common-fish').split(', ');
-            const legendaryFish = pin.getAttribute('data-legendary-fish').split(', ');
-
-            pinTitle.textContent = title;
-            pinDescription.textContent = description;
-
-            commonFishList.innerHTML = '';
-            legendaryFishList.innerHTML = '';
-
-            commonFish.forEach(fish => {
-                const li = document.createElement('li');
-                li.textContent = fish;
-                commonFishList.appendChild(li);
-            });
-
-            legendaryFish.forEach(fish => {
-                const li = document.createElement('li');
-                li.textContent = fish;
-                legendaryFishList.appendChild(li);
-            });
-
-            overlay.style.display = 'block';
-            pinInfoBox.style.display = 'block';
-            document.body.classList.add('no-scroll');
+        legendaryFish.forEach(fish => {
+            const li = document.createElement('li');
+            li.textContent = fish;
+            legendaryFishList.appendChild(li);
         });
-    });
 
-    closeBtn.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        pinInfoBox.style.display = 'none';
-        document.body.classList.remove('no-scroll');
-    });
-
-    overlay.addEventListener('click', () => {
-        overlay.style.display = 'none';
-        pinInfoBox.style.display = 'none';
-        document.body.classList.remove('no-scroll');
+        overlay.style.display = 'block';
+        pinInfoBox.style.display = 'block';
+        document.body.classList.add('no-scroll');
     });
 });
+
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    pinInfoBox.style.display = 'none';
+    document.body.classList.remove('no-scroll');
+});
+
+overlay.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    pinInfoBox.style.display = 'none';
+    document.body.classList.remove('no-scroll');
+});
+
